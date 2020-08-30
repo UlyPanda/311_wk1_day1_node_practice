7837bc115750ada3fe15991227ea5aa5c6a4a03a
UlyPanda <ulypanda44@gmail.com>
2020-08-30 09:06:22 -0600

    tests passing

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,20 @@
 // import moment here; use this package in each function
+const moment = require("moment");
+moment().format();
 
 const today = () => {
   // write code for dates.today
-
+moment().format('[today] dddd');
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+moment().format('MMM:DD:YYYY');
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+moment().format('hh:mm:ss a');
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+let arrOfPairs = [];
+for(let i = 0; i < str.length; i += 2){
+  arrOfPairs.push(str.slice(i, i + 2));
+}
+return arrOfPairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+let splitStr = str.split('');
+let revStr = splitStr.reverse();
+let joinStr = revStr.join('');
 
+return joinStr;
 }
 
 module.exports = {
